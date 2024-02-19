e73e930561e64156d0003e647b0bad7841258def
EsbenSE <s224555@student.dtu.dk>
2024-02-19 13:50:52 +0100

    setting og folder structure and testing json loading

---
+++ src/components/items.js
@@ -0,0 +1,17 @@
+import Products from './products.json'
+import 
+function getItems(){
+  return (
+    <div className="App">
+    
+     {
+      Products.map((data,i)=>(
+        
+      ))
+     }
+    
+    </div>
+  );
+  
+}
+export default getItem
