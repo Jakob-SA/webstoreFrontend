6887b97e5b3bae76ed35300bba3d63dc63f153f0
Jakob-SA <agergaardskovjakob@gmail.com>
2024-02-25 19:39:55 +0100

    Added giftwrap and changed the ID's naming scheme

--- src/assets/scripts/productLine.tsx
+++ src/assets/scripts/productLine.tsx
@@ -14,17 +14,23 @@ interface productLineProps {
 
 export function ProductLine({ quantity, product ,totalRebate, inStock }: productLineProps) {
     const [totalPrice, setTotalPrice] = useState(product.price * quantity);
+    const [giftwrapping, setGiftwrapping] = useState(false);
     
 
     const onQuantityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTotalPrice(product.price * parseInt(event.target.value));
-        console.log("Tryk");
+    }
+    const onGiftwrappingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setGiftwrapping(event.target.checked);
+    }
+    const removeLineItem = () => {
+        
     }
 
     return (
         <tr>  
             <td>
-                <div><img src={"src/assets/media/productPics/product" + product.id + ".jpg"} width="150" height="150" /></div>
+                <div><img src={"src/assets/media/productPics/product" + product.id + ".jpg"} className = "productImages" width="150" height="150" /></div>
             </td> 
             <td>
                 <div>{product.name}</div>
@@ -33,16 +39,20 @@ export function ProductLine({ quantity, product ,totalRebate, inStock }: product
             <div>{product.price}</div>
             </td>
             <td>
-            <div><input type="number" id={product.id.toString()} defaultValue={1} onChange={onQuantityChange}/></div>
+            <div><input type="number" id={`Quantity-${product.id.toString()}`} defaultValue={1} onChange={onQuantityChange}/></div>
             </td>
             <td>
             <div>{totalPrice.toFixed(2)}</div>
             </td>
             <td>
-            <div><button>Remove</button></div>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>        {/*shoukd be moved*/}
+                <input type="checkbox" id={`Giftwrapping-${product.id.toString()}`} onChange={onGiftwrappingChange} />
+                <label htmlFor={`Giftwrapping-${product.id.toString()}`}>Giftwrapping</label>
+                </div>
             </td>
-            
-            
+            <td>
+            <div><button onClick={removeLineItem}>Remove</button></div>
+            </td>
         </tr>
         
     )
