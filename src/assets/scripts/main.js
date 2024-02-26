6deb050411a0bede205263310c1f1560fc92de4f
Jakob-SA <agergaardskovjakob@gmail.com>
2024-02-26 12:40:36 +0100

    main.js deleted to see if this removes depricationerror

--- src/assets/scripts/main.js
+++
@@ -1,4 +0,0 @@
-//document.getElementById('antal').addEventListener('change', event => {
-//    let price = event.target.value * 100;
-//    document.getElementById('price').textContent = price.toString() + ' kr'
-//} )
