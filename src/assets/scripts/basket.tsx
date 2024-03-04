8dd4ba6ce97cf53d6a235543be2cdf88960b33ce
Jakob-SA <agergaardskovjakob@gmail.com>
2024-03-04 16:50:59 +0100

    Rezise pictures and fix all errors in console

--- src/assets/scripts/basket.tsx
+++ src/assets/scripts/basket.tsx
@@ -2,49 +2,48 @@ import { useState } from "react";
 import productArray from "./product";
 import { ProductLine } from "./productLine";
 
-
-
-
-
-
 function Basket() {
   const [basketItems, setBasketItems] = useState(productArray);
-  const handleRemoveItem = (id : number) => {
-    setBasketItems(prevItems => prevItems.filter(item => item.id !== id));
-  }
+  const handleRemoveItem = (id: number) => {
+    setBasketItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
 
- return (
-  <>
-    <h1>Checkout</h1>
-    <h2>Shopping cart</h2>
-    {basketItems.length > 0 && (
-    <table className="shoppingCart">
-      <tbody>
-      <tr>
-        <th>imgplaceholder</th>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th>Total</th>
-        <th></th>
-      </tr>
-      {basketItems.map((product) => {
-        return (
-        <ProductLine
-          key={product.id}
-          quantity={1} // replace with actual quantity
-          product={product}
-          handleRemoveItem={handleRemoveItem}
-        />
-        );
-      })}
-      </tbody>
-    </table>
-    )}
-    {basketItems.length === 0 && <p>No items in basket. Reload the page <a href=".">here</a> to restore</p>}
-    <p>Total Price: {}</p>
-    <p />
-  </>
+  return (
+    <>
+      <h1>Checkout</h1>
+      <h2>Shopping cart</h2>
+      {basketItems.length > 0 && (
+        <table className="shoppingCart">
+          <tbody>
+            <tr>
+              <th>imgplaceholder</th>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              <th></th>
+            </tr>
+            {basketItems.map((product) => {
+              return (
+                <ProductLine
+                  key={product.id}
+                  quantity={1} // replace with actual quantity
+                  product={product}
+                  handleRemoveItem={handleRemoveItem}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+      {basketItems.length === 0 && (
+        <p>
+          No items in basket. Reload the page <a href=".">here</a> to restore
+        </p>
+      )}
+      <p>Total Price: {}</p>
+      <p />
+    </>
   );
 }
 
