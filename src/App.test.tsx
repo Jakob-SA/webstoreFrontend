7d4827d120d510bbf0e97722a7c9bc5b9ec76f2c
Philip <philipmuff@gmail.com>
2024-03-04 16:54:56 +0100

    Added total price test for a single product

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,22 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 import App from "./App";
+import { ProductLine } from "./assets/scripts/productLine";
+import productsData from './assets/media/products.json';
+import  { Product } from './assets/scripts/product';
 
 describe(App.name, () => {
-  it("should render", () => {
+  test("should render", () => {
     render(<App />);
     expect(screen.getByText("Checkout")).toBeInTheDocument();
   });
 });
+
+describe(ProductLine.name, () => {
+    test("Should show correct total: 179.98", () => {
+        const product: Product = productsData[0]
+        const quantity = 2
+        render(<ProductLine quantity={quantity} product={product} handleRemoveItem={() => {}} />)
+        expect(screen.getByText(`${product.price * quantity}`)).toBeInTheDocument();
+    })
+})
