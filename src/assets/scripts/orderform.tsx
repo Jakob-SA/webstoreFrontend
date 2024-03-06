34fd93f67f9df3dbd7ecff774a5798c8dbd3df52
Jossemand <s224564@student.dtu.dk>
2024-03-07 00:22:38 +0100

    Start på styling

--- src/assets/scripts/orderform.tsx
+++ src/assets/scripts/orderform.tsx
@@ -2,47 +2,69 @@ import "./orderform.css";
 
 function Orderform() {
   return (
-    <form action="/my-handling-form-page" method="post">
+    <form action="/shipping-information" method="post">
+      <h1>Checkout</h1>
+
+      <h2>Shipping</h2>
       <legend>Enter your shipping details</legend>
       <ul>
         <select id="businessCustomer" required name="businessCustomer">
           <option value="private">Private</option>
           <option value="business">Business</option>
         </select>
-        <li>
-          <label htmlFor="name">Name:</label>
-          <input type="text" required id="name" name="user_name" />
-        </li>
-        <li>
-          <label htmlFor="adress1">Adress:</label>
-          <input type="text" required id="adress1" name="user_adress1" />
-        </li>
-        <li>
-          <label htmlFor="adress2">Appartment, suite etc. *Optional*:</label>
-          <input type="text" id="adress2" name="user_adress2" />
-        </li>
-        <li>
-          <label htmlFor="zip">Zip Code:</label>
-          <input type="text" required id="zip" name="user_zip" />
-        </li>
-        <li>
-          <label htmlFor="city">City:</label>
-          <input type="text" required id="city" name="user_city" />
-        </li>
-        <li>
-          <label htmlFor="email">Email:</label>
-          <input type="email" required id="email" name="user_email" />
-        </li>
-        <li>
-          <label htmlFor="tel">Phonenumber:</label>
-          <input
-            type="tel"
-            required
-            pattern="^(?:\+45|0045)?\s?\d{2}(?:\s?|\-?)\d{2}(?:\s?|\-?)\d{2}(?:\s?|\-?)\d{2}$"
-            name="user_telephoneNumber"
-          ></input>
-        </li>
-        <input type="submit" value="Place order" />
+        <section>
+          <div className="input-group">
+            <label htmlFor="first-name">First Name:</label>
+            <input
+              type="text"
+              required
+              id="first-name"
+              name="user_first_name"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="last-name">Last Name:</label>
+            <input type="text" required id="last-name" name="user_last_name" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="adress1">Adress:</label>
+            <input type="text" required id="adress1" name="user_adress1" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="adress2">Appartment, suite etc. *Optional*:</label>
+            <input type="text" id="adress2" name="user_adress2" />
+          </div>
+          <div className="address-checkbox">
+            <label>Different shipping address</label>
+            <input type="checkbox"></input>
+          </div>
+          <div className="input-group">
+            <label htmlFor="zip">Zip Code:</label>
+            <input type="text" required id="zip" name="user_zip" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="city">City:</label>
+            <input type="text" required id="city" name="user_city" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" required id="email" name="user_email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="tel">Phonenumber:</label>
+            <input
+              type="tel"
+              required
+              pattern="^(?:\+45|0045)?\s?\d{2}(?:\s?|\-?)\d{2}(?:\s?|\-?)\d{2}(?:\s?|\-?)\d{2}$"
+              name="user_telephoneNumber"
+            ></input>
+          </div>
+        </section>
+        <section>
+          <div>
+            <input type="submit" value="Place order" />
+          </div>
+        </section>
       </ul>
     </form>
   );
