6a667ed4c30e3d6184f5dc86e4bfd943f8c89cae
Philip <philipmuff@gmail.com>
2024-03-09 18:36:28 +0100

    Changed constants

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
-import App from "./App";
-import { ProductLine } from "./assets/scripts/productLine";
-import productsData from './assets/media/products.json';
-import  { Product } from './assets/scripts/product';
+import { render, screen } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+import App from "./App"
+import { ProductLine } from "./assets/scripts/productLine"
+import productsData from './assets/media/products.json'
+import  { Product } from './assets/scripts/product'
 
 describe(App.name, () => {
   test("should render", () => {
-    render(<App />);
-    expect(screen.getByText("Checkout")).toBeInTheDocument();
-  });
-});
+    render(<App />)
+    expect(screen.getByText("Checkout")).toBeInTheDocument()
+  })
+})
 
 describe(ProductLine.name, () => {
     test("Should show correct total: 179.98", () => {
         const product: Product = productsData[0]
         const quantity = 2
+        const totalPrice = product.price * quantity
         render(<ProductLine quantity={quantity} product={product} handleRemoveItem={() => {}} />)
-        expect(screen.getByText(`${product.price * quantity}`)).toBeInTheDocument();
+        expect(screen.getByText(totalPrice)).toBeInTheDocument()
     })
 })
