5bb4966b62db31b192c2e853b577a2c5b08c60ca
Philip <philipmuff@gmail.com>
2024-03-11 14:46:09 +0100

    Test for zip code implemented with mockresponse. Test is currently skipped

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,14 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import { describe, expect, test, vi } from "vitest"
+import { fireEvent, getByTestId, render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { afterEach, describe, expect, test, vi } from "vitest"
 import App from "./App"
 import { ProductLine } from "./assets/scripts/productLine"
 import productsData from './assets/media/products.json'
 import  { Product } from './assets/scripts/product'
 import { RemoveButton } from "./assets/scripts/removeButton"
 import { QuantityInput } from "./assets/scripts/quantityInput"
-import ZipCodeChecker from "./assets/scripts/zipCodeChecker"
 import Orderform from "./assets/scripts/orderform"
+import mockResponse from "./assets/media/mockResponse.json"
 
 describe(App.name, () => {
   test("should render", () => {
@@ -17,6 +18,9 @@ describe(App.name, () => {
 })
 
 describe(ProductLine.name, () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
   test("Should decrement quantity from 3 to 2", () => {
       const product: Product = productsData[0]
       const quantity = 3
@@ -38,15 +42,28 @@ describe(RemoveButton.name, () => {
   })
 })
 
-describe(ZipCodeChecker.name, async () => {
-  test.skip("Should return as valid zipcode", () => {
+describe(Orderform.name, () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  test.skip("Should return the corrosponding city for zip code", async () => {
+    const user = userEvent.setup()
     const mockFetch = vi.spyOn(window, "fetch").mockImplementation(async () => {
       return {
-        json: async () => Response,
+        json: async () => mockResponse,
       } as Response
     })
+
     render(<Orderform/>)
-    const form =  screen.getByTestId("zip")
+    
+    const zipCodeInput = screen.getByLabelText("Zip Code:")
+    await user.type(zipCodeInput, "2200")
+    const form = screen.getByTestId("zip-data-form")
     fireEvent.submit(form)
+    
+    await screen.findByText("København N")
+    expect(mockFetch).toHaveBeenCalledWith(
+      "https://api.dataforsyningen.dk/postnumre/2200"
+    )
   })
 })
