1e9ba346919953d1f65a8657d3fb7fa8e09f2b55
Philip <philipmuff@gmail.com>
2024-03-11 16:12:15 +0100

    Merge conflicts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,23 +16,6 @@ describe(App.name, () => {
     expect(screen.getByText("Basket")).toBeInTheDocument
   })
 })
-=======
-import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
-import App from "./App";
-import { ProductLine } from "./assets/scripts/productLine";
-import productsData from "./assets/media/products.json";
-import { Product } from "./assets/scripts/product";
-import { RemoveButton } from "./assets/scripts/removeButton";
-import { QuantityInput } from "./assets/scripts/quantityInput";
-
-describe(App.name, () => {
-  test("should render", () => {
-    render(<App />);
-    expect(screen.getByText("Checkout")).toBeInTheDocument();
-  });
-});
->>>>>>> Dev
 
 describe(ProductLine.name, () => {
   afterEach(() => {
@@ -51,7 +34,6 @@ describe(ProductLine.name, () => {
 
 describe(RemoveButton.name, () => {
   test("Should change the basketicon on hover", () => {
-<<<<<<< HEAD
     render(<RemoveButton onClick={() => {}}/>)
     const button = screen.getByRole('button')
     const openTrashcanImage  = screen.getByRole('img') as HTMLImageElement
@@ -60,6 +42,7 @@ describe(RemoveButton.name, () => {
   })
 })
 
+// Test doesn't seem to be able to work. We might need to look into how we input the city into forms.
 describe(Orderform.name, () => {
   afterEach(() => {
     vi.restoreAllMocks()
@@ -74,23 +57,14 @@ describe(Orderform.name, () => {
 
     render(<Orderform/>)
     
-    const zipCodeInput = screen.getByLabelText("Zip Code:")
+    const zipCodeInput = screen.getByLabelText("Zip Code:*")
     await user.type(zipCodeInput, "2200")
-    const form = screen.getByTestId("zip-data-form")
-    fireEvent.submit(form)
+    fireEvent.blur(zipCodeInput)
     
     await screen.findByText("København N")
+
     expect(mockFetch).toHaveBeenCalledWith(
       "https://api.dataforsyningen.dk/postnumre/2200"
     )
   })
 })
-=======
-    render(<RemoveButton onClick={() => {}} />);
-    const button = screen.getByRole("button");
-    const openTrashcanImage = screen.getByRole("img") as HTMLImageElement;
-    fireEvent.mouseOver(button);
-    expect(openTrashcanImage.src).toContain("openTrashcan.png");
-  });
-});
->>>>>>> Dev
