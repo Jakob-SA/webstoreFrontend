b1e86fd156b8a2c7fa463d10cdfad44f4ba54151
Jakob-SA <agergaardskovjakob@gmail.com>
2024-04-18 21:27:46 +0200

    Footer added. doesnt look nice

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Basket from "./components/basket/basket";
 import OrderSummary from "./components/basket/orderSummary";
 import UpsellItem from "./components/basket/upsellItem";
+import Footer from "./components/header/footer";
 import Header from "./components/header/header";
 import NavigationBar from "./components/header/NavigationBar";
 
@@ -19,6 +20,7 @@ function App() {
           <OrderSummary />
         </aside>
       </div>
+      <Footer />
     </>
   );
 }
