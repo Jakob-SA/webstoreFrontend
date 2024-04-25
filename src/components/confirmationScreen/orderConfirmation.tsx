f7264ac5a3985c9d8b97c3e21a2a1e5d99fda31e
Jossemand <s224564@student.dtu.dk>
2024-04-25 22:29:42 +0200

    Relevant price is shown in confirmation screen

--- src/components/confirmationScreen/orderConfirmation.tsx
+++ src/components/confirmationScreen/orderConfirmation.tsx
@@ -1,7 +1,9 @@
 import { useShopContext } from "../../contexts/shopContext";
+import { getTotalPrice } from "../basket/totalPrice";
 
 function OrderConfirmation({ orderNumber }: { orderNumber: number }) {
   const { basketItems } = useShopContext();
+  const totalPrice = getTotalPrice();
 
   const basketLines = basketItems.map((item) => (
     <tr key={item.product.id}>
@@ -9,7 +11,7 @@ function OrderConfirmation({ orderNumber }: { orderNumber: number }) {
         <p>{item.product.name}</p>
       </td>
       <td>
-        <p>{item.product.price}</p>
+        <p>{item.product.price * item.quantity}</p>
       </td>
       <td>
         <p>{item.quantity}</p>
@@ -28,12 +30,12 @@ function OrderConfirmation({ orderNumber }: { orderNumber: number }) {
           <tbody>
             <tr>
               <th>Product</th>
-              <th>Price per unit</th>
+              <th>Price</th>
               <th>Quantity</th>
             </tr>
             {basketLines}
           </tbody>
-          <p>Subtotal: $</p>
+          <p>Subtotal: ${totalPrice.toFixed(2)}</p>
         </table>
         <p>Your order details have been sent to your email.</p>
       </div>
