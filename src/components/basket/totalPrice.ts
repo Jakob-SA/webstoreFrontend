c5d9988b5c0f2cf8a113b870d76e291c3d9f9252
Esben <esbenskov@live.dk>
2024-04-27 10:35:03 +0200

    totalPrice should work now with rebate for each productLine. also added a getDiscountAmount to get the amount of discount

--- src/components/basket/totalPrice.ts
+++ src/components/basket/totalPrice.ts
@@ -5,28 +5,33 @@ function calculateDiscount(totalPrice: number): number {
     if (totalPrice > 300) {
       return totalPrice * 0.9;
     } else {
-
       return totalPrice;
     }
   }
 
+  export const getDiscountAmount = () => {
+    const totalPrice = getTotalPrice();
+    return (totalPrice / 0.9 - totalPrice).toFixed(2)
+  }
+
   //TODO: also make a function to return the total discount amount.
-  // (totalPrice / 0.9 - totalPrice).toFixed(2)
+  // 
 
 export const getTotalPrice = () => {
 
     const {basketItems} = useShopContext();
-    /* TODO: make this return the correct total price and rebate is calculated correctly.
-    It does seem to work now though.
+   
+    //Array of all the original line prices without rebate
+    var originalLinePrice = basketItems.map((item)=>item.quantity*item.product.price) ;
 
-    const product = basketItems.map(item => item.product);
-    var originalLinePrice = product.forEach() * item.quantity;
-    const totalPrice = basketItems.reduce((sum, item) => 
-        item.quantity >= item.product.rebateQuantity
-    ? originalLinePrice * (1 - item.product.rebatePercent / 100)
-    : originalLinePrice, 0);
-*/
-    const totalPrice = basketItems.reduce((sum, item) => 
-        sum + (item.totalLinePrice*1-(item.product.rebatePercent/100)), 0); // still not working 
+    //total price of all productlines and takes rebate into account. Co pilot helped here.
+    const totalPrice = basketItems.reduce((sum, item, index) => {
+      const price = item.quantity >= item.product.rebateQuantity
+    ? originalLinePrice[index] * (1 - item.product.rebatePercent / 100)
+    : originalLinePrice[index]
+    return sum + price;
+  }, 0);
+    console.log(totalPrice)
     return(calculateDiscount(totalPrice))
+   
 }
