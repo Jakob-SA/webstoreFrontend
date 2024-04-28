b80f9ff47fa79f5dfb813b87042213c52d3d7939
Jakob-SA <agergaardskovjakob@gmail.com>
2024-04-28 18:11:46 +0200

    Made upsellItems nice and styled with button to add to basket. Button not working.

--- src/components/basket/upsellItem.tsx
+++ src/components/basket/upsellItem.tsx
@@ -1,35 +1,50 @@
-import "./basket.css";
+import "./upsellItem.css";
 import { useShopContext } from "../../contexts/useShopContext";
 
 export function UpsellItem() {
   const { products, basketLines } = useShopContext();
-  const basketItemIds = basketLines.map(item => item.product.id);
+  const basketItemIds = basketLines.map((item) => item.product.id);
   const upsellItems = products.filter(
-    product => !basketItemIds.includes(product.id)
+    (product) => !basketItemIds.includes(product.id)
   );
-  
+
   if (upsellItems.length === 0 || products.length === 0) {
     return <div>Loading...</div>;
   }
-  
+
   return (
     <>
-      <section className="upsellItems">
+      <div>
         <h2>Products you might also like!</h2>
-        {upsellItems.slice(0,3).map((item, index) => (
-          <div key={index}>
-            <img
-              src={"productPics/product" + item.id + ".jpg"}
-              className="productImages"
-              width="150"
-            />
-            <p>{item.price ? item.price : 'finding price'} $</p>
-            <p>{item.name}</p>
-          </div>
-        ))}
-      </section>
+        <section className="upsellItemsContainer">
+          {upsellItems.slice(0, 3).map((item, index) => (
+            <div className="flip-card" key={index}>
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    src={"productPics/product" + item.id + ".jpg"}
+                    className="upsellImages"
+                    width="150"
+                  />
+                  <div className="upsellItemInformation">
+                    <p>{item.name}</p>
+                    <p>{item.price ? item.price : "finding price"} $</p>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <button onClick={() => {}}>ADD TO BASKET</button>
+                  <img
+                    src={"productPics/product" + item.id + ".jpg"}
+                    className="upsellImages"
+                    width="150"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
     </>
   );
- 
 }
 export default UpsellItem;
