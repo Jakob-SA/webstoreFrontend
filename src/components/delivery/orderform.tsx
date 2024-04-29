e8699f0258cb70fae0bd0f531dbd032322bbb928
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2024-04-29 14:39:26 +0200

    fixed businessorder issue

--- src/components/delivery/orderform.tsx
+++ src/components/delivery/orderform.tsx
@@ -187,7 +187,30 @@ function Orderform() {
     <>
       <form onSubmit={handleSubmit}>
         <FormHeader />
-        <BusinessOrder />
+        {isBusiness && (
+          <div className="duoBox">
+            <div className="input-wrapper" data-required>
+              <input
+                type="text"
+                required
+                id="businessName"
+                name="user_businessName"
+              />
+              <label htmlFor="businessName">Business Name</label>
+            </div>
+            <div className="input-wrapper" data-required>
+              <input
+                type="text"
+                required
+                id="VAT"
+                name="user_VAT"
+                pattern="^\d{8}$" // This is a pattern to validate the VAT number to the danish format
+                title="Please enter a valid danish VAT number"
+              />
+              <label htmlFor="VAT">Vat Number</label>
+            </div>
+          </div>
+        )}
         <div className="duoBox">
           <div className="input-wrapper" data-required>
             <input
